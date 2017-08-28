c93adc82d4cb9d4b9ebab169bab4f92162e7c2de
Francisco Noé Acevedo Rivera <paconoeacevedo@gmail.com>
2017-08-28 14:21:16 -0500

    Configuraciones para trabajar
    localmente como root y htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://codiga.mx/';
+$config['base_url'] = 'http://fnar.com:8087/win/dev-cod/';
 
 /*
 |--------------------------------------------------------------------------
